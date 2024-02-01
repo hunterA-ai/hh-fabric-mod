83b5c7da50a33d67a811468a9141afbabc45c4e9
hunterA-ai <huntersteenanderson@gmail.com>
2024-02-01 07:29:38 -0800

    keybinds added, chat funcitonality non-funcitonal

--- src/client/java/com/hunterhack/HunterHackClient.java
+++ src/client/java/com/hunterhack/HunterHackClient.java
@@ -1,10 +1,13 @@
 package com.hunterhack;
 
+
 import net.fabricmc.api.ClientModInitializer;
 
 public class HunterHackClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+
+		com.hunterhack.event.KeyInputHandler.register();
 	}
 }
---
+++ src/client/java/com/hunterhack/event/KeyInputHandler.java
@@ -0,0 +1,41 @@
+package com.hunterhack.event;
+
+import org.lwjgl.glfw.GLFW;
+
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
+import net.minecraft.text.TextContent;
+import net.minecraft.text.TranslatableTextContent;
+import net.minecraft.text.MutableText;
+
+
+// TODO: Test this class
+public class KeyInputHandler {
+    public static final String KEY_CATEGORY_HunterHack = "key.category.hhmod.keybinds";
+    public static final String KEY_GEN_TEXT = "key.hhmod.gentext";
+
+    public static KeyBinding textGenKeyBinding;
+    public static String generatedText = "I put the fun in functionality!";
+
+    public static void registerKeyInputs() {
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if(textGenKeyBinding.wasPressed()) {
+                // This happens when our custom key is pressed
+                client.player.sendMessage(Text.translatable(generatedText));
+            }
+        });
+    }
+
+
+    public static void register() {
+        textGenKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            KEY_GEN_TEXT,
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_O,
+            KEY_CATEGORY_HunterHack
+            ));
+    }
+}
